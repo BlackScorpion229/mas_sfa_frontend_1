c7b4595bd3b94fd79b119940d3de0530f488ea21
Black Scorpion 229 <64971651+BlackScorpion229@users.noreply.github.com>
2025-12-24 21:37:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["yc6pmx-8080.csb.app"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
